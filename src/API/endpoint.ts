b90fa3da7d9b9ef094288d165f60faf631d7706c
devopati <davidopati70>
2025-04-14 09:51:37 +0300

    new commit

--- src/API/endpoint.ts
+++ src/API/endpoint.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-export const BACKEND_URL = "http://192.168.0.11:3000/api/tradeaxis/";
-export const SOCKET_URL = "http://192.168.0.11:3000";
-// export const BACKEND_URL = "https://tradeorbit-server.onrender.com/api/tradeaxis/";
-// export const SOCKET_URL = "https://tradeorbit-server.onrender.com";
+// export const BACKEND_URL = "http://192.168.0.11:3000/api/tradeaxis/";
+// export const SOCKET_URL = "http://192.168.0.11:3000";
+export const BACKEND_URL =
+  "https://tradeorbit-server.onrender.com/api/tradeaxis/";
+export const SOCKET_URL = "https://tradeorbit-server.onrender.com";
 
 export const API = axios.create({
   baseURL: BACKEND_URL,
